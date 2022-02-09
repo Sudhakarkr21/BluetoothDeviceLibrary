e509335f6a10fa06173b9b3edc9ec36f194bed16
Sudhakar <sudhakarkr21@gmail.com>
2022-02-09 10:39:06 +0530

    Second Commit

--- app/src/main/java/com/example/bluetoothdevicelibrary/test/TestDummy1.java
+++
@@ -1,7 +0,0 @@
-package com.example.bluetoothdevicelibrary.test;
-
-import com.example.bluetoothdevicelibrary.test1.TestDummy2;
-
-public class TestDummy1 extends TestDummy2 {
-    protected int k = 0;
-}
--- app/src/main/java/com/example/bluetoothdevicelibrary/test1/TestDummy2.java
+++
@@ -1,5 +0,0 @@
-package com.example.bluetoothdevicelibrary.test1;
-
-public class TestDummy2 {
-    protected int testing = 0;
-}
