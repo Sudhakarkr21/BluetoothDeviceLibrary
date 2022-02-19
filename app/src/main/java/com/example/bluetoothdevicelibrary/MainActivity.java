1f2fccf1ba809fa44ad32129309741d757eb3a8d
ChetanG <cgani87@gmail.com>
2022-02-19 14:49:27 +0530

    testing the git commands

--- app/src/main/java/com/example/bluetoothdevicelibrary/MainActivity.java
+++ app/src/main/java/com/example/bluetoothdevicelibrary/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity
             });
         });
 
+        String test = "chetan";
+
         findViewById(R.id.selectMeter).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
